4205b1e149fe7bc91828783269429096e1a005e7
Joenn <zzjoennzz@gmail.com>
2022-07-31 14:37:21 +0800

    fixed exporting

--- src/api/phAddress.js
+++ src/api/phAddress.js
@@ -1,46 +1,12 @@
-const allRegion = require('./region.json');
-const allProvince = require('./province.json');
-const allCity = require('./city.json');
 const allBarangay = require('./barangay.json');
 
-const fetch = (jsonName) => {
-    switch (jsonName) {
-        case "region":
-            return allRegion;
-
-        case "province":
-            return allProvince
-
-        case "city":
-            return allCity;
-
-        case "barangay":
-            return allBarangay;
-
-        default:
-            break;
-    }
+const fetch = () => {
+    return allBarangay;
 }
 
-const regions = () => {
+export const barangays = async (code = "0314") => {
     try {
-        const regs = fetch("region");
-        return regs.map((region) => {
-            return {
-                id: region.id,
-                psgc_code: region.psgc_code,
-                region_name: region.region_name,
-                region_code: region.region_code,
-            }
-        })
-    } catch (error) {
-        return error;
-    }
-}
-
-const barangays = async (code = "0314") => {
-    try {
-        const brgy = fetch("barangay");
+        const brgy = fetch();
         return brgy.filter(barangay => barangay.city_code === code).map((filtered) => {
             return {
                 id: filtered.brgy_code,
@@ -54,5 +20,3 @@ const barangays = async (code = "0314") => {
         return error;
     }
 }
-
-module.exports = { regions, barangays };
